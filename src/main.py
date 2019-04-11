100a12399e9e4626ef130920c99cf786d410fb78
Aaron Mekonnen <meko0019@umn.edu>
2019-04-10 21:21:21 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+
+try:
+	api_key = os.environ.get(API_KEY)
+	api_secret = os.environ.get(API_SECRET)
+except Exception as e:
+	return 
+
+client = Client(api_key, api_secret)
